7323ea796de76467b7273621056aee5f61bb77fb
Martin Bothe <martin.bothe@quasiris.de>
2026-03-17 13:56:31 +0100

    Updated binds.nix to seperate between work and home

--- modules/core/work/packages.nix
+++ modules/core/work/packages.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    (teams-for-linux.override { electron_39 = electron_39-bin; }) # Microsoft Teams
+    (teams-for-linux.override {electron_39 = electron_39-bin;}) # Microsoft Teams
     electron_40-bin # Cross platform desktop shell
     xdg-utils # Needed for opening links and better integration
     thunderbird-latest-unwrapped # Thunderbird Email client
