15b58ee30cffc1836afd5af77e6e04bc340e172c
Martin Bothe <martin.bothe@quasiris.de>
2026-03-24 10:49:21 +0100

    moved config

--- modules/user/work/ssh.nix
+++ modules/user/work/ssh.nix
@@ -1,10 +1,20 @@
-{ ... }: {
+{...}: {
   programs.ssh = {
     enable = true;
     matchBlocks = {
       "github.com" = {
         hostname = "github.com";
         identityFile = "~/.ssh/id_ed25519_github";
+      };
+      "gitlab" = {
+        hostname = "gitlab.quasiris.de";
+        port = 2222;
+        user = "mbr";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+      "*.quasiris.de" = {
+        user = "mbr";
+        identityFile = "~/.ssh/id_ed25519";
       };
     };
   };
